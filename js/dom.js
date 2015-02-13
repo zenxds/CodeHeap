8bdb6b320e00c47ddddb0e931b430753769ca8da
zenxds <zenxds@gmail.com>
2015-02-13 10:33:13 +0800

    add f2e

---
+++ js/dom.js
@@ -0,0 +1,9 @@
+// element.insertAdjacentHTML(position, text);
+// beforeBegin：在该元素前插入
+// afterBegin：在该元素第一个子元素前插入
+// beforeEnd：在该元素最后一个子元素后面插入
+// afterEnd：在该元素后插入
+
+// createElement
+// appendChild
+// removeChild
