b30a3296a15186950e06d6dade3cd5a1dbfe6532
zenxds <zenxds@gmail.com>
2017-11-23 10:45:08 +0800

    support webp

--- js/browser-support.js
+++ js/browser-support.js
@@ -1,3 +1,21 @@
+const supportWebp = new Promise((resolve, reject) => {
+	const img = new Image()
+	
+	img.onload = function() {
+		if(img.height > 0 && img.width > 0) {
+			resolve()
+		} else {
+			reject()
+		}
+	}
+
+	img.onerror = function() {
+		reject()
+	}
+
+	img.src = 'data:image/webp;base64,UklGRkoAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAsAAAABBxAREYiI/gcAAABWUDggGAAAADABAJ0BKgEAAQABABwlpAADcAD+/gbQAA=='
+})
+
 var canvasElem = document.createElement('canvas');
 
 var support = {
