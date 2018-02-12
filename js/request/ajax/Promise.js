62c45e12801853fa75381eadbba2847a7f99e3cf
zenxds <zenxds@gmail.com>
2018-02-12 14:55:19 +0800

    promise to es6

--- js/request/ajax/Promise.js
+++ js/request/ajax/Promise.js
@@ -3,15 +3,12 @@
  * https://promisesaplus.com/
  */
 
-var PENDING = 0
-var FULFILLED = 1
-var REJECTED = 2
+const PENDING = 0
+const FULFILLED = 1
+const REJECTED = 2
 
-function Promise(executor) {
-    if (!(this instanceof Promise)) {
-        return new Promise(executor)
-    }
-
+class Promise {
+  constructor(executor) {
     this._state = PENDING
     this._onFulfilled = []
     this._onRejected = []
@@ -21,8 +18,87 @@ function Promise(executor) {
     this._reason = null
 
     if (isFunction(executor)) {
-        executor(bind(this.resolve, this), bind(this.reject, this))
+      executor((...args) => {
+        this.resolve(...args)
+      }, (...args) => {
+        this.reject(...args)
+      })
     }
+  }
+
+  then(onFulfilled, onRejected) {
+    const promise = new Promise()
+
+    this._onFulfilled.push(wrapper(promise, onFulfilled, 'resolve'))
+    this._onRejected.push(wrapper(promise, onRejected, 'reject'))
+
+    this.flush()
+    return promise
+  }
+
+  flush() {
+    let state = this._state
+
+    if (state === PENDING) {
+      return
+    }
+
+    const fns = state === FULFILLED ? this._onFulfilled.slice() : this._onRejected.slice()
+    const arg = state === FULFILLED ? this._value : this._reason
+
+    setTimeout(function() {
+      each(fns, function(fn) {
+        try {
+          fn(arg)
+        } catch (err) {}
+      })
+    }, 0)
+
+    this._onFulfilled = []
+    this._onRejected = []
+  }
+
+  resolve(value) {
+    if(this._state !== PENDING) {
+      return
+    }
+
+    this._state = FULFILLED
+    this._value = value
+
+    this.flush()
+  }
+
+  reject(reason) {
+    if (this._state !== PENDING) {
+      return
+    }
+
+    this._state = REJECTED
+    this._reason = reason
+
+    this.flush()
+  }
+
+  isPending() {
+    return this._state === PENDING
+  }
+
+  isFulfilled() {
+    return this._state === FULFILLED
+  }
+
+  isRejected() {
+    return this._state === REJECTED
+  }
+
+  catch(onRejected) {
+    return this.then(null, onRejected)
+  }
+
+  finally(onFinally) {
+    return this.then(onFinally, onFinally)
+  }
 }
 
 /**
@@ -30,213 +106,116 @@ function Promise(executor) {
  * https://promisesaplus.com/#point-47
  */
 function resolve(promise, x) {
-    if (promise === x) {
-        promise.reject(new TypeError('A promise cannot be resolved with itself.'))
-        return
-    }
+  if (promise === x) {
+    promise.reject(new TypeError('A promise cannot be resolved with itself.'))
+    return
+  }
 
-    if (isThenable(x)) {
-        try {
-            x.then(function(value) {
-                resolve(promise, value)
-            }, function(reason) {
-                promise.reject(reason)
-            })
-        } catch (e) {
-            promise.reject(e)
-        }
-    } else {
-        promise.resolve(x)
+  if (isThenable(x)) {
+    try {
+      x.then(function(value) {
+        resolve(promise, value)
+      }, function(reason) {
+        promise.reject(reason)
+      })
+    } catch (err) {
+      promise.reject(err)
     }
+  } else {
+    promise.resolve(x)
+  }
 }
 
 function wrapper(promise, fn, actionType) {
-    return function(val) {
-        if (isFunction(fn)) {
-            try {
-                var x = fn(val)
-                resolve(promise, x)
-            } catch (e) {
-                promise.reject(e)
-            }
-        } else {
-            // https://promisesaplus.com/#point-43
-            // val is the _reason or _value of the origin promise
-            promise[actionType](val)
-        }
+  return function(val) {
+    if (isFunction(fn)) {
+      try {
+        let x = fn(val)
+        resolve(promise, x)
+      } catch (err) {
+        promise.reject(err)
+      }
+    } else {
+      // https://promisesaplus.com/#point-43
+      // val is the _reason or _value of the origin promise
+      promise[actionType](val)
     }
-}
-
-Promise.prototype = {
-    constructor: Promise,
-
-    then: function(onFulfilled, onRejected) {
-        var promise = new Promise()
-
-        this._onFulfilled.push(wrapper(promise, onFulfilled, 'resolve'))
-        this._onRejected.push(wrapper(promise, onRejected, 'reject'))
-
-        this.flush()
-        return promise
-    },
-
-    flush: function() {
-        var state = this._state
-
-        if (state === PENDING) {
-            return
-        }
-
-        var fns = state === FULFILLED ? this._onFulfilled.slice() : this._onRejected.slice()
-        var arg = state === FULFILLED ? this._value : this._reason
-
-        setTimeout(function() {
-            each(fns, function(fn) {
-                try {
-                    fn(arg)
-                } catch (e) {
-                    void 0
-                }
-            })
-        }, 0)
-
-
-        this._onFulfilled = []
-        this._onRejected = []
-    },
-
-    resolve: function(value) {
-        if (this._state !== PENDING) {
-            return
-        }
-
-        this._state = FULFILLED
-        this._value = value
-
-        this.flush()
-    },
-
-    reject: function(reason) {
-        if (this._state !== PENDING) {
-            return
-        }
-
-        this._state = REJECTED
-        this._reason = reason
-
-        this.flush()
-    },
-
-    isPending: function() {
-        return this._state === PENDING
-    },
-
-    isFulfilled: function() {
-        return this._state === FULFILLED
-    },
-
-    isRejected: function() {
-        return this._state === REJECTED
-    },
-
-    'catch': function(onRejected) {
-        return this.then(null, onRejected)
-    },
-
-    always: function(onAlways) {
-        return this.then(onAlways, onAlways)
-    }
+  }
 }
 
 Promise.defer = function() {
-    var deferred = {}
+  const deferred = {}
 
-    deferred.promise = new Promise(function(resolve, reject) {
-        deferred.resolve = resolve
-        deferred.reject = reject
-    })
+  deferred.promise = new Promise(function(resolve, reject) {
+    deferred.resolve = resolve
+    deferred.reject = reject
+  })
 
-    return deferred
+  return deferred
 }
 
 Promise.race = function(arr) {
-    var defer = Promise.defer()
-    var len = arr.length
-    var results = []
+  const defer = Promise.defer()
 
-    each(arr, function(promise) {
-        promise.then(function(value) {
-            defer.resolve(value)
-        }, function(reason) {
-            defer.reject(reason)
-        })
+  each(arr, function(promise) {
+    promise.then(function(value) {
+      defer.resolve(value)
+    }, function(reason) {
+      defer.reject(reason)
     })
-    return defer.promise
+  })
+
+  return defer.promise
 }
 
 Promise.all = function(arr) {
-    var defer = Promise.defer()
-    var length = arr.length
-    var results = []
+  const defer = Promise.defer()
+  const results = []
+  let length = arr.length
 
-    each(arr, function(promise, i) {
-        promise.then(function(value) {
-            results[i] = value
-            length--
+  each(arr, function(promise, i) {
+    promise.then(function(value) {
+      results[i] = value
+      length--
 
-            if (length === 0) {
-                defer.resolve(results)
-            }
-        }, function(reason) {
-            defer.reject(reason)
-        })
+      if (length === 0) {
+        defer.resolve(results)
+      }
+    }, function(reason) {
+      defer.reject(reason)
     })
-    return defer.promise
+  })
+
+  return defer.promise
 }
 
 Promise.resolve = function(value) {
-    return new Promise(function(resolve) {
-        resolve(value)
-    })
+  return new Promise(function(resolve) {
+    resolve(value)
+  })
 }
 
 Promise.reject = function(reason) {
-    return new Promise(function(resolve, reject) {
-        reject(reason)
-    })
+  return new Promise(function(resolve, reject) {
+    reject(reason)
+  })
 }
 
 function isThenable(val) {
-    return val && isFunction(val.then)
+  return val && isFunction(val.then)
 }
 
-function bind(fn, context) {
-    var slice = [].slice
-    var args = slice.call(arguments, 2)
-    var noop = function() {}
-    var ret = function() {
-        return fn.apply(this instanceof noop ? this : context, args.concat(slice.call(arguments)))
-    }
-
-    noop.prototype = fn.prototype
-    ret.prototype = new noop()
-    return ret
-}
-
-var isFunction = isType('Function')
+const isFunction = isType('Function')
 function isType(type) {
-    return function(obj) {
-        return {}.toString.call(obj) == "[object " + type + "]"
-    }
+  return function(obj) {
+    return {}.toString.call(obj) == "[object " + type + "]"
+  }
 }
 
 function each(arr, fn) {
-    var i = 0
-    var length = arr.length
-
-    for (; i < length; i++) {
-        fn(arr[i], i)
-    }
+  for (let i = 0; i < arr.length; i++) {
+    fn(arr[i], i)
+  }
 }
 
-module.exports = Promise
+export default Promise
