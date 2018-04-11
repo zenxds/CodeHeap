4d9a61d084684f3e9fd4003008b45c321afb6d90
zenxds <zenxds@gmail.com>
2018-04-11 16:58:29 +0800

    pattern

---
+++ js/pattern/next/index.js
@@ -0,0 +1,41 @@
+/**
+ * 职责链模式
+ */
+Function.prototype.after = function(fn) {
+  return (...args) => {
+    let ret = this(...args)
+    if (ret === 'next') {
+      return fn(...args)
+    }
+
+    return ret
+  }
+}
+
+const head = document.head || document.getElementsByTagName('head')[0]
+
+function getCurrentScript1() {
+  return document.currentScript || 'next'
+}
+
+function getCurrentScript2() {
+  let scripts = head.getElementsByTagName("script")
+
+  for (let i = scripts.length - 1; i >= 0; i--) {
+    let script = scripts[i]
+    if (script.readyState === "interactive") {
+      return interactiveScript
+    }
+  }
+
+  return 'next'
+}
+
+function getCurrentScript3() {
+  let scripts = head.getElementsByTagName("script")
+
+  return scripts[scripts.length - 1]
+}
+
+let getCurrentScript = getCurrentScript1.after(getCurrentScript2).after(getCurrentScript3)
+console.log(getCurrentScript())
